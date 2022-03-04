def582ac579d30c40ea872ffb99e33bdb04c93c5
elakkiyadineshkumar <99124487+elakkiyadineshkumar@users.noreply.github.com>
2022-03-04 14:42:47 +0530

    firstjava

---
+++ first.java
@@ -0,0 +1,11 @@
+public class Swap{
+public static void main(String args[]){
+String str1="Rithik";
+String str2="Sanjana";
+str1=str1+str2; 
+system.out.println(str1)
+str2=str1.subString(str1.lenght-str2.length) // 13-7 -> 6 
+str1=str1.substring(0,str2.length)
+system.out.println("After Swap:" + str1)
+system.out.println("After Swap:" + str2)
+}
